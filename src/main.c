f00121b346caf7ee1119d25fc5ed0783871c945e
kamite0611 <kamiteakira@gmail.com>
2024-07-30 02:24:05 +0900

    save

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: akamite <akamite@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 18:46:23 by akamite           #+#    #+#             */
-/*   Updated: 2024/07/30 02:01:13 by akamite          ###   ########.fr       */
+/*   Updated: 2024/07/30 02:23:57 by akamite          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,6 @@ int	main(int argc, char *argv[])
 	initialize_cub_data(&cub_data, argv[1]);
 	init_cub_data(&cub_data);
 	// mlx_loop(cub_data.mlx);
-	system("leaks -atExit -- ./cub3D ");
+	// system("leaks -atExit -- ./cub3D");
 	return (0);
 }
