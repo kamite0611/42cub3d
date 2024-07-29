5d8dc3325fa1f7b74c05e256cc16a191cbc523d8
kamite0611 <kamiteakira@gmail.com>
2024-07-29 20:30:40 +0900

    save

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: akamite <akamite@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 18:46:23 by akamite           #+#    #+#             */
-/*   Updated: 2024/07/29 20:11:41 by akamite          ###   ########.fr       */
+/*   Updated: 2024/07/29 20:30:35 by akamite          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 
 int main(int argc, char *argv[])
 {
+	t_cub_data cub_data;
+
 	(void)argv;
+	(void)cub_data;
 	if (argc != 2)
 		return (err_msg(ERR_USAGE, 1));
+
+
 
 	return 0;
 }
