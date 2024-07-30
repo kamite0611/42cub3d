bfac1f31be8c39faf2f792fce852449ff079a4c7
kamite0611 <kamiteakira@gmail.com>
2024-07-31 01:23:31 +0900

    save

--- src/utils/debug.c
+++ src/utils/debug.c
@@ -6,7 +6,7 @@
 /*   By: akamite <akamite@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 01:18:50 by akamite           #+#    #+#             */
-/*   Updated: 2024/07/31 01:22:01 by akamite          ###   ########.fr       */
+/*   Updated: 2024/07/31 01:23:21 by akamite          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	put_str_arr(char **str_arr, char *name)
 	i = 0;
 	while (str_arr[i])
 	{
-		printf("%s[%d]\t: \"%s\" ,\n", name, i, str_arr[i]);
+		printf("%s[%d]\t: \"%s\"\n", name, i, str_arr[i]);
 		i++;
 	}
 }
@@ -27,11 +27,11 @@ void	put_str_arr(char **str_arr, char *name)
 void	put_mapinfo(t_mapinfo *mapinfo)
 {
 	printf("\n################## mapinfo ##################\n");
-	printf("path\t\t: \"%s\" \n", mapinfo->path);
-	printf("no_path\t\t: \"%s\" \n", mapinfo->no_path);
-	printf("so_path\t\t: \"%s\" \n", mapinfo->so_path);
-	printf("we_path\t\t: \"%s\" \n", mapinfo->we_path);
-	printf("ea_path\t\t: \"%s\" \n", mapinfo->ea_path);
+	printf("path\t\t: \"%s\"\n", mapinfo->path);
+	printf("no_path\t\t: \"%s\"\n", mapinfo->no_path);
+	printf("so_path\t\t: \"%s\"\n", mapinfo->so_path);
+	printf("we_path\t\t: \"%s\"\n", mapinfo->we_path);
+	printf("ea_path\t\t: \"%s\"\n", mapinfo->ea_path);
 	printf("line_count\t: %d\n", mapinfo->line_count);
 	printf("floor_rgb\t: R[%d], G[%d], B[%d]\n", mapinfo->floor_rgb[0],
 		mapinfo->floor_rgb[1], mapinfo->floor_rgb[2]);
