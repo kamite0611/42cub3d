c38de86157213cc9a0e3847b41e638230139f379
kamite0611 <kamiteakira@gmail.com>
2024-08-10 22:45:03 +0900

    save

--- includes/cub3D.h
+++ includes/cub3D.h
@@ -6,7 +6,7 @@
 /*   By: akamite <akamite@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 20:05:10 by akamite           #+#    #+#             */
-/*   Updated: 2024/08/10 22:17:14 by akamite          ###   ########.fr       */
+/*   Updated: 2024/08/10 22:37:29 by akamite          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,9 @@ typedef struct s_img
 	int		endian;
 }			t_img;
 
+/**
+ * テクスチャ情報
+ */
 typedef struct s_texinfo
 {
 	int		**tex_north;
--- src/exit/free_exit.c
+++ src/exit/free_exit.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   free_exit.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mnakashi <mnakashi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akamite <akamite@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/29 21:56:49 by akamite           #+#    #+#             */
-/*   Updated: 2024/08/04 19:09:30 by mnakashi         ###   ########.fr       */
+/*   Updated: 2024/08/10 22:44:44 by akamite          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,21 @@ static void	free_mapinfo(t_mapinfo *mapinfo)
 }
 
 /**
+ *
+ */
+static void	free_texinfo(t_texinfo *texinfo)
+{
+	if (texinfo->tex_north)
+		free_tab((void **)texinfo->tex_north);
+	if (texinfo->tex_south)
+		free_tab((void **)texinfo->tex_south);
+	if (texinfo->tex_west)
+		free_tab((void **)texinfo->tex_west);
+	if (texinfo->tex_south)
+		free_tab((void **)texinfo->tex_south);
+}
+
+/**
  * t_gameの中身でmallocしているものをfreeする
  */
 static void	free_game(t_game *game)
@@ -37,6 +52,7 @@ static void	free_game(t_game *game)
 	if (game)
 	{
 		free_mapinfo(&game->mapinfo);
+		free_texinfo(&game->texinfo);
 		free_tab((void **)game->view_pixels);
 	}
 }
