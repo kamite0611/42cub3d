ffc1aee7e5ee83caba254b9f724db24b8f63bb4d
kamite0611 <kamiteakira@gmail.com>
2024-08-12 17:28:03 +0900

    save

--- src/init/init_img.c
+++ src/init/init_img.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   init_img.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mnakashi <mnakashi@student.42.fr>          +#+  +:+       +#+        */
+/*   By: akamite <akamite@student.42tokyo.jp>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/30 02:10:38 by akamite           #+#    #+#             */
-/*   Updated: 2024/08/11 13:53:20 by mnakashi         ###   ########.fr       */
+/*   Updated: 2024/08/12 17:26:46 by akamite          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,49 +57,11 @@ void	initialize_img(t_img *image)
 void	init_texture_img(t_game *game, t_img *image, char *path, t_temp *temp)
 {
 	initialize_img(image);
-	image->img = mlx_xpm_file_to_image(game->mlx, path, &temp->texture_size, &temp->texture_size);
+	image->img = mlx_xpm_file_to_image(game->mlx, path, &temp->texture_size,
+			&temp->texture_size);
 	if (image->img == NULL)
 		free_exit(game, err_msg(ERR_MLX, 1));
 	image->addr = (int *)mlx_get_data_addr(image->img, &image->pixel_bits,
 			&image->size_line, &image->endian);
 	return ;
-}
-
-static int	*xpm_to_img(t_game *game, char *path, t_temp *temp)
-{
-	t_img	tmp;
-	int		*buffer;
-	int		x;
-	int		y;
-
-	init_texture_img(game, &tmp, path, temp);
-	buffer = ft_calloc(1, sizeof * buffer * 64 * 64);
-	if (!buffer)
-		free_exit(game, err_msg(ERR_MALLOC, 1));
-	y = 0;
-	while (y < 64)
-	{
-		x = 0;
-		while (x < 64)
-		{
-			buffer[y * 64 + x] = tmp.addr[y * 64 + x];
-			++x;
-		}
-		y++;
-	}
-	mlx_destroy_image(game->mlx, tmp.img);
-	return (buffer);
-}
-
-
-int	init_texture(t_game *game, t_temp *temp)
-{
-	game->textures = ft_calloc(5, sizeof * game->textures);
-	if (!game->textures)
-		free_exit(game, err_msg(ERR_MALLOC, 1));
-	game->textures[0] = xpm_to_img(game, game->mapinfo.no_path, temp);
-	game->textures[1] = xpm_to_img(game, game->mapinfo.so_path, temp);
-	game->textures[2] = xpm_to_img(game, game->mapinfo.ea_path, temp);
-	game->textures[3] = xpm_to_img(game, game->mapinfo.we_path, temp);
-	return (SUCCESS);
 }
